0107c79a1b328ab9ab91a6794f83696a6d0f9f1c
ericlong12 <114244136+ericlong12@users.noreply.github.com>
2025-03-07 13:20:00 -0800

    Added Basic UI
    The start game menu implemented.
    
    Still need to add game mode button "regular" and " party"

---
+++ Assets/Scripts/PlayerCountSelector.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;  
+
+public class PlayerCountSelector : MonoBehaviour
+{
+    [SerializeField] private Slider playerCountSlider;
+    [SerializeField] private TMP_Text playerCountText; 
+
+    private void Start()
+    {
+        // Initialize display
+        UpdatePlayerCountDisplay(playerCountSlider.value);
+
+        // Listen for slider changes
+        playerCountSlider.onValueChanged.AddListener(UpdatePlayerCountDisplay);
+    }
+
+    private void UpdatePlayerCountDisplay(float value)
+    {
+        int playerCount = Mathf.RoundToInt(value);
+        playerCountText.text = playerCount + " Players";
+
+        // Store player count in a global manager, player prefs, or directly into a GameManager
+        PlayerPrefs.SetInt("PlayerCount", playerCount);  
+    }
+}
